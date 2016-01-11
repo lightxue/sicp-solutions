99e07c0fdc5f64f9df1fc6e534e7ff17576a45b2
lightxue <bkmgtp@gmail.com>
2016-01-11 21:01:17 +0800

    10

---
+++ 10.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+
+(A 1 10)
+
+(A 2 4)
+
+(A 3 3)
+
+
+(define (f n) (A 0 n))
+; 简化
+(define (f n)
+  (* 2 n))
+
+(define (g n) (A 1 n))
+; 简化
+(define (g n)
+  (cond ((= n 0) 0)
+        ((= n 1) 2)
+        (else (* 2 (g (- n 1))))))
+; 2的n次方
+
+
+(define (h n) (A 2 n))
+; 简化
+(define (h n)
+  (cond ((= n 0) 0)
+        ((= n 1) 2)
+        (else (g (h (- n 1))))))
+; 2^(2^(2^2^(...)))
+
+
+
